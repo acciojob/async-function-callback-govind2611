c747e65b264c441af017cea00132f0170bf70a59
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-06-11 09:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+const btn = document.querySelector('#btn');
+const output = document.querySelector('#output');
 
-//your JS code here. If required.
+
+const fetchData = async() => {
+	const res = await fetch(url);
+	const data = await res.json();
+	output.innerText = data.title;
+}
+
+btn.addEventListener('click', fetchData);
